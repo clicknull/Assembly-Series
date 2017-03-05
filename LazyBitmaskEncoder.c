3e3602265bdecc1b023ab593b0aa0d61deb3e092
ApertureSecurity <Tatrasiel7@gmail.com>
2017-03-05 10:00:42 -0700

    Update LazyBitmaskEncoder.c

--- LazyBitmaskEncoder.c
+++ LazyBitmaskEncoder.c
@@ -12,19 +12,18 @@ then print a pattern in assembly will De-XOR them back then place them on the st
 
 int main() {
 
-
-int LengthOfShellCode = 332;// Please set this to meet your needs. I sometimes have 0x00 bytes in my code , 
-int XOR_Byte = 0xEE;	//so getting a length isn't always easy. 
+// Please set this to meet your needs. I sometimes have 0x00 bytes in my code ,so getting a length isn't always easy.
+int LengthOfShellCode = 332;
+int XOR_Byte = 0xEE;
+int XORNOP = (0x90 ^ XOR_Byte);
 
 
 unsigned char ReversedByteArray[LengthOfShellCode];
-//unsigned char Xor_Array[LengthOfShellCode];
 unsigned char XOR_bytearray[LengthOfShellCode];
 
 
 unsigned char bytearray[] = \
-"SHELLCODE GOES HERE"
-
+"SHELLCODE GOES HERE";
 
 
 
@@ -53,15 +52,31 @@ void MakeBackwardsArray(unsigned char Normal_Array[], bool debug){
 
 void Preamble(){ printf("mov edx, %X%X \n", XOR_Byte,XOR_Byte);}
 
+void Warning(){/*if(LengthOfShellCode % 2 != 0)*/
+    printf("\n\n;[*] Warning --- Keep in mind that your shellcode needs to divide evenly into the size of a DWORD . Fill the remaining bytes with (0x90 ^ XOR byte)\n");
+    printf(";The last MOV EAX instruction should be: \n\n;mov eax, FFFF%02X%02X \n\n;If you have an extra byte after this, Please consider replacing it with an XOR'd 0x90 , In this case, it's %02X\n", (bytearray[1] ^XOR_Byte) ,(bytearray[0]^XOR_Byte),XORNOP );
+    printf(";You might need to add the following before you 'jmp esp'\n\n");
+	printf("MOV EBX,%02X%02X \n", XORNOP,XORNOP);
+	printf("MOV AX,BX \n");
+	printf("XOR AX,DX \n");
+	printf("PUSH EAX \n\n\n");
+
+    }
+
+
+
 void PrintShellPayload(bool debug){ // In general this function still requires some attention as it might not provide an complete amount of instructions in the end.
-    if( debug == true) {for(int i = 0 ; i < LengthOfShellCode; i++){    printf("%02X",XOR_bytearray[i]); }}
+    if( debug == true) {for(int i = 0 ; i < LengthOfShellCode; i++){ printf("%02X",XOR_bytearray[i]); }}
     else{
         for(int i = 0; i <= LengthOfShellCode; i++){
         printf("mov eax, FFFF%02X%02X \n", XOR_bytearray[i],XOR_bytearray[i+1]);
         printf("xor ax, dx\n");
         printf("and eax, FFFF \n");
         printf("rol eax, 0x10 \n"); // this moves ax to the first 4 bits of EAX . like so 0000FFFF to FFFF0000;
-        if( i == LengthOfShellCode){printf("push eax \n");break;} // This might not be needed
+            if( i == LengthOfShellCode){
+                    printf("push eax \n");
+                    Warning();
+                    break;} // This might not be needed
 
         i +=2;
 
@@ -91,9 +106,10 @@ ORIG FC :: 12
 */
 
 
+void Finally(){ printf("\n\njmp esp\n\n");}
 
-void Finally(){ printf("jmp esp\n");}
-void Warning(){/*if(LengthOfShellCode % 2 != 0)*/ printf(";[+] Warning --- Keep in mind that your shellcode needs to be the size of a DWORD . Fill the remaining bytes with (0x90 ^ XOR byte) \n In this case, it's %02X \n", (0x90 ^ XOR_Byte));}
+
+
 
 
 MakeBackwardsArray(bytearray,false);
@@ -103,8 +119,10 @@ XORShellcode(ReversedByteArray,LengthOfShellCode,XOR_Byte, false);
 
 Preamble();
 PrintShellPayload(false);
+
+
 Finally();
-Warning();
 return 0;
 
 }
+
